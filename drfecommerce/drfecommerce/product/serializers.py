2cdcf6bcdb64e42437282461afb4b3fffcfd8eed
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-06-10 13:03:08 +0100

    ch: added brand url

--- drfecommerce/drfecommerce/product/serializers.py
+++ drfecommerce/drfecommerce/product/serializers.py
@@ -5,7 +5,7 @@ from .models import Brand, Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ['name']
         
         
         
@@ -17,6 +17,8 @@ class BrandSerializer(serializers.ModelSerializer):
         
         
 class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    category = CategorySerializer()
     class Meta:
         model = Product
         fields = "__all__"
