1e413fbd6b356d093fc706603bf09034aed142ee
Tomi Ogunsan <tomiafolayan@gmail.com>
2024-06-10 10:46:00 +0100

    ch: added schema and swagger

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
+from drf_spectacular.utils import extend_schema
 
-# Create your views here.
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = Category.objects.all()
+    
+    
+    @extend_schema(responses=CategorySerializer)
+    def list(self, request):
+        serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
